355c9f0b4dbd9b72659ef5f0d64c8c8c947990f7
Joshua Brewton <jebrewton@gmail.com>
2016-07-12 17:36:52 -0400

    week 1 submission

---
+++ readme.md
@@ -0,0 +1,15 @@
+# Assignment 1
+Your overall score for this assignment is 9.98 out of 10.00
+
+
+Your solution passed all of our tests, congratulations! You obtained the maximal test
+score of 8.00.
+
+Our automated style checker tool found issues in your code with respect to coding style: it
+computed a style score of 1.98 out of 2.00 for your submission. See below for detailed feedback.
+
+======== CODING STYLE ISSUES ========
+Checking file /grader/repository/courses/progfun1/src/main/scala/common/package.scala... OK!
+Checking file /grader/repository/courses/progfun1/src/main/scala/recfun/Main.scala...
+  1. warning.text at line 35 character 10:
+     return.message
--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -13,15 +13,53 @@ object Main {
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = ???
+    def pascal(c: Int, r: Int): Int = {
+            
+      // beginning and ends are 1
+      if ( c == 0 || c == r ) {
+        1
+      } else {
+        // internal values are the previous rows sum of current and prior
+        pascal(c-1, r-1) + pascal(c, r-1)
+      }
+    }
   
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
+    def balance(chars: List[Char]): Boolean = {
+      def innerBalance(acc: Int, idx: Int): Boolean = {
+        
+        //if we are at the end or there is a close before an open then we escape
+        if (idx == chars.length || acc < 0) {
+          return acc == 0
+        }
+        
+        //count up when we open, count down when we close
+        if (chars(idx) == '(') {
+          innerBalance(acc+1, idx+1)
+        } else if (chars(idx) == ')' ) {
+          innerBalance(acc-1, idx+1)
+        } else {
+          innerBalance(acc, idx+1)
+        }
+      }
+      
+      innerBalance(0, 0)
+    }
   
   /**
    * Exercise 3
    */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    def countChange(money: Int, coins: List[Int]): Int = {
+      
+      if (money == 0) {
+        1
+      } else if (money > 0 && !coins.isEmpty) {
+        // give change for the leading coin, give change for the remaining coins
+        countChange(money - coins.head, coins) + countChange(money, coins.tail)
+      } else {
+        0
+      }
+    }
   }
